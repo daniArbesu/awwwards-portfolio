a566b1d40d9aff55b875b438ee6005e268b7e640
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-29 12:47:23 +0200

    fix: add link to logo

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,15 +1,16 @@
+import Link from 'next/link';
 import styles from './style.module.css';
 
 const Logo = () => {
   return (
-    <div className={styles.logoWrapper}>
+    <Link href="/" className={styles.logoWrapper}>
       <p className={styles.copyright}>©</p>
       <div className={styles.logo}>
         <p className={styles.codeBy}>Code by</p>
         <p className={styles.name}>Daniel</p>
         <p className={styles.surname}>Arbesú</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
