3d500595ff9e5c470141965772d9303a0e8f1277
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-29 13:05:51 +0200

    chore: abstract icon to its own component

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -6,6 +6,7 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import { motion } from 'framer-motion';
 import { slideUp } from './animation';
+import { HeroArrow } from '@/components/ui/icons/Index';
 
 const HeroSection = () => {
   const firstTextRef = useRef<HTMLParagraphElement>(null);
@@ -55,12 +56,7 @@ const HeroSection = () => {
         </div>
       </div>
       <div data-scroll data-scroll-speed={0.1} className={styles.description}>
-        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
-            fill="white"
-          />
-        </svg>
+        <HeroArrow />
         <p>Freelance</p>
         <p>Designer & Developer</p>
       </div>
