c5277b3655a1dd1ddf32b0a3d948a1840b94a176
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-29 13:48:01 +0200

    feat: add animation to button

--- src/components/ui/HamburgerButton/index.tsx
+++ src/components/ui/HamburgerButton/index.tsx
@@ -1,4 +1,7 @@
+import { useLayoutEffect, useRef } from 'react';
 import styles from './style.module.css';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 interface Props {
   isActive: boolean;
@@ -6,8 +9,27 @@ interface Props {
 }
 
 const HamburgerButton: React.FC<Props> = ({ isActive, setIsActive }) => {
+  const hamburgerRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(hamburgerRef.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: '+=300px',
+        onLeave: () => {
+          gsap.to(hamburgerRef.current, { scale: 1, duration: 0.25, ease: 'power1.out' });
+        },
+        onEnterBack: () => {
+          gsap.to(hamburgerRef.current, { scale: 0, duration: 0.25, ease: 'power1.out' });
+        }
+      }
+    });
+  }, []);
+
   return (
-    <div className={styles.buttonWrapper}>
+    <div className={styles.buttonWrapper} ref={hamburgerRef}>
       <button
         className={styles.button}
         onClick={() => {
