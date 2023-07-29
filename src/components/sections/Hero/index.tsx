74027948bda0a74622ce65de14e9e40327194c22
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-29 11:16:48 +0200

    feat: add hero section

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -1,7 +1,71 @@
+'use client';
+import Image from 'next/image';
 import styles from './style.module.css';
+import { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { motion } from 'framer-motion';
+import { slideUp } from './animation';
 
 const HeroSection = () => {
-  return <section className={styles.hero}>index</section>;
+  const firstTextRef = useRef<HTMLParagraphElement>(null);
+  const secondTextRef = useRef<HTMLParagraphElement>(null);
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  let xPercent = 0;
+  let direction = -1;
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(sliderRef.current, {
+      ScrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: 0.25,
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        onUpdate: (e: ScrollTrigger) => (direction = e.direction * -1)
+      },
+      x: '-500px'
+    });
+
+    requestAnimationFrame(animation);
+  }, []);
+
+  const animation = () => {
+    if (xPercent <= -100) {
+      xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstTextRef.current, { xPercent });
+    gsap.set(secondTextRef.current, { xPercent });
+    requestAnimationFrame(animation);
+    xPercent += 0.1 * direction;
+  };
+
+  return (
+    <motion.section variants={slideUp} initial="initial" animate="animate" className={styles.hero}>
+      <Image src={'/images/hero-background.avif'} alt="Hero Background Portrait" fill={true} />
+      <div className={styles.sliderContainer}>
+        <div ref={sliderRef} className={styles.slider}>
+          <p ref={firstTextRef}>Freelance Developer -</p>
+          <p ref={secondTextRef}>Freelance Developer -</p>
+        </div>
+      </div>
+      <div data-scroll data-scroll-speed={0.1} className={styles.description}>
+        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
+            fill="white"
+          />
+        </svg>
+        <p>Freelance</p>
+        <p>Designer & Developer</p>
+      </div>
+    </motion.section>
+  );
 };
 
 export default HeroSection;
