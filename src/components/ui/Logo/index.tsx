241929b3be47cd62ed6b77db8134f6e080e301be
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-30 20:21:48 +0200

    feat: add magnetic effect to logo

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,16 +1,19 @@
 import Link from 'next/link';
 import styles from './style.module.css';
+import MagneticEffect from '../MagneticEffect';
 
 const Logo = () => {
   return (
-    <Link href="/" className={styles.logoWrapper}>
-      <span className={styles.copyright}>©</span>
-      <div className={styles.logo}>
-        <span className={styles.codeBy}>Code by</span>
-        <span className={styles.name}>Daniel</span>
-        <span className={styles.surname}>Arbesú</span>
-      </div>
-    </Link>
+    <MagneticEffect>
+      <Link href="/" className={styles.logoWrapper}>
+        <span className={styles.copyright}>©</span>
+        <div className={styles.logo}>
+          <span className={styles.codeBy}>Code by</span>
+          <span className={styles.name}>Daniel</span>
+          <span className={styles.surname}>Arbesú</span>
+        </div>
+      </Link>
+    </MagneticEffect>
   );
 };
 
