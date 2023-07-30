97b309bf9ce6635cf385c38f4ab20c7c648128b5
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-30 20:23:05 +0200

    feat: add magnetic effect to navbar

--- src/components/ui/Nav/index.tsx
+++ src/components/ui/Nav/index.tsx
@@ -1,15 +1,18 @@
 import { navLinks } from '@/constants/cmsInfo';
 import styles from './style.module.css';
 import Link from 'next/link';
+import MagneticEffect from '../MagneticEffect';
 
 const Nav = () => {
   return (
     <ul className={styles.nav}>
       {navLinks.slice(1).map(({ title, href }, index) => (
-        <li className={styles.el} key={index}>
-          <Link href={href}>{title}</Link>
-          <div className={styles.indicator}></div>
-        </li>
+        <MagneticEffect key={index}>
+          <li className={styles.el}>
+            <Link href={href}>{title}</Link>
+            <div className={styles.indicator}></div>
+          </li>
+        </MagneticEffect>
       ))}
     </ul>
   );
