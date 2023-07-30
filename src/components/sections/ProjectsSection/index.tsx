815e1953cea698c95407faf72939e4cb88d7b4a5
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-30 14:24:56 +0200

    feat: add section title and containers

--- src/components/sections/ProjectsSection/index.tsx
+++ src/components/sections/ProjectsSection/index.tsx
@@ -9,6 +9,7 @@ const ProjectsSection = () => {
 
   return (
     <section className={styles.projects}>
+      <h2 className={styles.sectionTitle}>Recent Work</h2>
       <ul className={styles.container}>
         {projects.map(({ title, type, href }, index) => (
           <Project
